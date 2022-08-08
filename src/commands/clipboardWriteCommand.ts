46b75ab7c230c64781f9d135cfc27a8249df253c
Tiago Tristao <tiago.v.tristao@gmail.com>
2022-08-08 10:43:14 +0100

    Display target on a ssh connection

--- src/commands/clipboardWriteCommand.ts
+++ src/commands/clipboardWriteCommand.ts
@@ -5,6 +5,14 @@ export function clipboardWriteCommand(args: {
   text: string;
   message?: string;
 }): void {
+  // Fallback to displaying the target onto the information box,
+  // because access to the clipboard will be from the context
+  // of the server when ssh'ing.
+  if (process.env.SSH_CLIENT) {
+    vscode.window.showInformationMessage(args.text);
+    return;
+  }
+
   clipboardy.writeSync(args.text);
 
   const message = args.message || `Copied onto the clipboard: '${args.text}`;
